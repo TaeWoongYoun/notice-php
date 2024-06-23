cc3c7878e74c110d4c0d0274c888f5516ce09921
xodnd0507 <24457545yong@gmail.com>
2024-06-23 12:56:54 +0900

    feat : Update page

--- update.php
+++ update.php
@@ -24,6 +24,17 @@
                 $row = mysqli_fetch_array($result);
             }
         ?>
+
+        <tr>
+            <form action="updateProcess.php" method="post">
+                <input type="hidden" name="id" value="<?= $row['id']?>">
+                <td><?= $row['id']?></td>
+                <td><input type="text" name="name" value="<?= $row['name'] ?>"></td>
+                <td><input type="text" name="title" value="<?= $row['title'] ?>"></td>
+                <td><p>자동완성</p></td>
+                <td><input type="submit" value="submit"></td>
+            </form>
+        </tr>
     </table>
     <script src="index.js"></script>
 </body>
