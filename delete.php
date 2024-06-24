4dbff3a8ffedb97b402e25e68a0522574c399715
xodnd0507 <24457545yong@gmail.com>
2024-06-24 09:45:36 +0900

    feat : Delete process

---
+++ delete.php
@@ -0,0 +1,11 @@
+<?php $conn = mysqli_connect('localhost', 'root', '', 'notice')?>
+<?php
+    $id = mysqli_real_escape_string($conn, $_POST['id']);
+    $sql = "DELETE FROM notice WHERE id = $id";
+    $result = mysqli_query($conn, $sql);
+?>
+
+<script>
+    alert('삭제 성공')
+    location.href = 'main.php'
+</script>
